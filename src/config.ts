d1ea51520937654902ca521cc49400d13db2b05a
aexer0e <aexeroe@gmail.com>
2025-09-17 04:21:44 +0300

    first commit

---
+++ src/config.ts
@@ -0,0 +1,60 @@
+export const SITE = {
+    title: "Eeb | Portfolio",
+    description: "A simple portfolio created with Astro",
+    tags: ["portfolio", "Resume cv"],
+    lang: "en",
+    favicon: "/favicon.png",
+    author: "Eeb",
+}
+
+export const ME = {
+    name: "Eeb",
+    profession: "Software Engineer | Game Developer",
+    profileImage: "pp2.png",
+    profileFacts: [
+        {
+            value: "8+",
+            description: "Years of Experience"
+        },
+        {
+            value: "20+",
+            description: "Completed Projects"
+        },
+        {
+            value: "10+",
+            description: "Satisfied Clients"
+        }
+    ],
+    contactInfo: {
+        email: "ibehawar@gmail.com",
+        linkedin: "https://www.linkedin.com/in/ibrahim-shaban/"
+    },
+    aboutMe: "I am a software engineer with a passion for web development. I have experience in building web " + "applications using modern technologies. I am a self-taught developer who enjoys learning new things and " + "sharing knowledge with others.",
+}
+
+export const SOCIALS = [
+    {
+        name: "X",
+        url: "https://x.com/aexer0e",
+        icon: "twitter-x-fill",
+        show: true
+    },
+    {
+        name: "GitHub",
+        url: "https://github.com/aexer0e",
+        icon: "github-fill",
+        show: true
+    },
+    {
+        name: "LinkdIn",
+        url: "https://www.linkedin.com/in/ibrahim-shaban",
+        icon: "linkedin-fill",
+        show: true
+    },
+    {
+        name: "Instagram",
+        url: "https://www.instagram.com/eeb.irl",
+        icon: "instagram-fill",
+        show: true
+    }
+]
